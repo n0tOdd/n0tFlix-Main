97d46a2c584ec9059cfca061a2df5cd50050972b
ersoucy <42904120+ersoucy@users.noreply.github.com>
2021-12-12 22:01:46 -0600

    commit

--- External/AngleSharp.Js/obj/Release/netstandard2.1/AngleSharp.Js.AssemblyInfo.cs
+++ External/AngleSharp.Js/obj/Release/netstandard2.1/AngleSharp.Js.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AngleSharp.Js")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbfdcee56c51a700a895a41ebb85d760bf98d009")]
 [assembly: System.Reflection.AssemblyProductAttribute("AngleSharp.Js")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AngleSharp.Js")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
